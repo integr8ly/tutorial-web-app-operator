b32aea4f5db67a495c1d350d04e88325e442b7be
Peter Braun <pbraun@redhat.com>
2020-07-28 09:54:53 +0200

    update webapp version to 2.27.0

--- pkg/handlers/webhandler.go
+++ pkg/handlers/webhandler.go
@@ -36,7 +36,7 @@ const (
 	ClusterTypeDefault        = "not set"
 	OpenShiftVersionDefault   = "3"
 	OpenShiftAPIHostDefault   = "openshift.default.svc"
-	WebAppImage               = "quay.io/integreatly/tutorial-web-app:2.26.3"
+	WebAppImage               = "quay.io/integreatly/tutorial-web-app:2.27.0"
 	serviceName               = "tutorial-web-app"
 	routeName                 = "tutorial-web-app"
 	upgradeData               = "UPGRADE_DATA"
